75463912f196fef75b0af7dd3a6cc4702866186c
Sávio_Francisco <saviofrancisco150@hotmail.com>
2022-04-05 21:46:47 -0300

    Frist commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    include './lib/listar.php';
+    $alunos= listar_alunos();
+    $alunoEsp= $GLOBALS["alunos"][6]
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+<body>
+    <header>
+        <a href="/">Home</a>
+        <a href="/listarAlunos.php">Alunos</a>
+    </header>
+    <main>
+        Hello!!
+    <?php
+
+        echo "<p>".$alunos[6]."</p>";
+    
+    ?>
+    </main>
+    
+</body>
+</html>
---
+++ lib/listar.php
@@ -0,0 +1,11 @@
+<?php
+
+    $alunos = ['Bianca ', 'Claudia ', 'Luiza ','Marcos ', 'Michele ','Pedro ', 'Sávio'];
+    
+    function listar_alunos(){
+
+            return $GLOBALS["alunos"];
+        }
+        //echo "$nome_aluno  "."$idade_pessoa";
+    listar_alunos();
+?>
